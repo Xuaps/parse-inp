d5005d4b71a9ef9337b691db13e8b9a7174fb095
David Vílchez <dvilchez@xuaps.com>
2023-05-30 20:12:23 +0200

    Read reservoir

--- packages/parser/src/lib.rs
+++ packages/parser/src/lib.rs
@@ -1,22 +1,37 @@
 use std::str::Chars;
 
+#[derive(Debug, PartialEq)]
 pub struct INP {
-    title: String
+    title: String,
+    reservoirs: Vec<RESERVOIR>
+}
+
+#[derive(Debug, PartialEq)]
+pub struct RESERVOIR {
+    id: String,
+    head: f64,
+    pattern: String
 }
 
 impl INP {
     pub fn read(content: &str) -> Self {
-        let mut inp = INP { title: String::new() };
+        let mut inp = INP { title: String::new(), reservoirs: Vec::new() };
         let mut chars = content.chars();
-        match chars.next() {
-            Some('[') => match inp.read_section(&mut chars).as_deref() {
-                Some("TITLE") => {
-                    inp.skip_line(&mut chars);
-                    inp.read_title(&mut chars)
+        while let Some(c) = chars.next() {
+            match c {
+                '[' => match inp.read_section(&mut chars).as_deref() {
+                    Some("TITLE") => {
+                        inp.skip_line(&mut chars);
+                        inp.read_title(&mut chars)
+                    },
+                    Some("RESERVOIRS") => {
+                        inp.skip_line(&mut chars);
+                        inp.read_reservoirs(&mut chars)
+                    },
+                    _ => panic!("Invalid section")
                 },
-                _ => panic!("Invalid section")
-            },
-            _ => panic!("Invalid INP file")
+                _ => panic!("Invalid INP file")
+            }
         }
         inp
     }
@@ -41,11 +56,50 @@ impl INP {
         self.title = title;
     }
 
+    pub fn read_reservoirs(&mut self, chars: &mut Chars) {
+        let mut reservoirs = Vec::new();
+        let mut c = chars.next();
+        while c != Some('[') {
+            let mut name = String::new();
+            while c != Some(' ') {
+                name.push(c.unwrap());
+                c = chars.next();
+            }
+            c = self.skip_spaces(chars);
+            let mut head = String::new();
+            while c != Some(' ') {
+                head.push(c.unwrap());
+                c = chars.next();
+            }
+            c = self.skip_spaces(chars);
+            let mut pattern = String::new();
+            while c != Some('\n') {
+                pattern.push(c.unwrap());
+                c = chars.next();
+            }
+            c = self.skip_spaces(chars);
+            reservoirs.push(RESERVOIR { 
+                id: name, 
+                head: head.parse::<f64>().unwrap(), 
+                pattern
+            });
+        }
+        self.reservoirs = reservoirs;
+    }
+
     pub fn skip_line(&mut self, chars: &mut Chars) {
         let mut c = chars.next();
         while c != Some('\n') {
             c = chars.next();
         }
+    }
+
+    pub fn skip_spaces(&mut self, chars: &mut Chars) -> Option<char> {
+        let mut c = chars.next();
+        while c == Some(' ') {
+            c = chars.next();
+        }
+        c
     }
 }
 
@@ -53,12 +107,27 @@ impl INP {
 #[cfg(test)]
 mod test {
     use super::INP;
+    use super::RESERVOIR;
 
     #[test]
     fn read_inp_title() {
         let input = "[TITLE]\nHello World\n";
         let inp = INP::read(input);
         assert_eq!(inp.title, "Hello World");
+    }
+
+    #[test]
+    fn read_inp_reservoirs() {
+        let input = "[TITLE]\nHello World\n[RESERVOIRS]\nR2     120       Pat1\n[";
+        let inp = INP::read(input);
+        assert_eq!(
+            inp.reservoirs, 
+            vec![RESERVOIR { 
+                id: "R2".to_string(), 
+                head: 120.0, 
+                pattern: "Pat1".to_string() 
+            }]
+        );
     }
 }
 
