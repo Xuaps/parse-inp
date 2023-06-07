f5a58f01ecf6f2787683b69bc42ec366d1d15395
David Vílchez <dvilchez@xuaps.com>
2023-06-07 17:02:39 +0200

    Add read sources

--- packages/parser/src/lib.rs
+++ packages/parser/src/lib.rs
@@ -2,6 +2,7 @@
 #[derive(Debug, PartialEq)]
 pub struct INP {
     title: String,
+    sources: Vec<SOURCE>,
     reservoirs: Vec<RESERVOIR>
 }
 
@@ -13,9 +14,18 @@ pub struct RESERVOIR {
     comment: Option<String>
 }
 
+#[derive(Debug, PartialEq)]
+pub struct SOURCE {
+    node: String,
+    source_type: String,
+    strength: f64,
+    pattern: Option<String>,
+    comment: Option<String>
+}
+
 impl INP {
     pub fn read(content: &str) -> Self {
-        let mut inp = INP { title: String::new(), reservoirs: Vec::new() };
+        let mut inp = INP { title: String::new(), sources: Vec::new(), reservoirs: Vec::new() };
         let mut lines = content.lines();
         let mut section = None;
         while let Some(line) = lines.next() {
@@ -27,6 +37,7 @@ impl INP {
                 Some(';') => continue,
                 _ => match section.as_deref() {
                         Some("TITLE") => inp.read_title_line(line),
+                        Some("SOURCES") => inp.read_sources(line),
                         Some("RESERVOIRS") => inp.read_reservoir(line),
                         other => panic!("Invalid section {}", other.unwrap_or(""))
                     }
@@ -79,12 +90,26 @@ impl INP {
         });
     }
 
+    fn read_sources(&mut self, line: &str) {
+        let mut parts = line.split(";");
+        let properties = parts.next().unwrap_or("").split_whitespace().collect::<Vec<&str>>();
+        let comment = parts.next();
+
+        let node = properties[0].to_string();
+        let source_type= properties[1].to_string();
+        let strength = properties[2].parse::<f64>().unwrap();
+        let pattern = if properties.len() > 3 { Some(properties[3].to_string()) } else { None };
+        let comment = comment.map(|s| s.to_string());
+
+        self.sources.push(SOURCE { node, source_type, strength, pattern, comment });
+    }
 }
 
 
 #[cfg(test)]
 mod test {
     use super::INP;
+    use super::SOURCE;
     use super::RESERVOIR;
 
     #[test]
@@ -117,6 +142,35 @@ mod test {
                     head: 120.0, 
                     pattern: Some("Pat1".to_string()),
                     comment: Some("Head varies with time".to_string())},
+            ]
+        );
+    }
+
+    #[test]
+    fn read_sources() {
+        let input = r#"
+            [SOURCES] 
+            ;Node  Type    Strength  Pattern 
+            ;-------------------------------- 
+            N1     CONCEN  1.2       Pat1    ;Concentration varies with time
+            N44    MASS    12                ;Constant mass injection
+        "#;
+        let inp = INP::read(input);
+        assert_eq!(
+            inp.sources, 
+            vec![
+                SOURCE { 
+                    node: "N1".to_string(), 
+                    source_type: "CONCEN".to_string(), 
+                    strength: 1.2,
+                    pattern: Some("Pat1".to_string()),
+                    comment: Some("Concentration varies with time".to_string()) },
+                SOURCE { 
+                    node: "N44".to_string(), 
+                    source_type: "MASS".to_string(), 
+                    strength: 12.0,
+                    pattern: None,
+                    comment: Some("Constant mass injection".to_string()) },
             ]
         );
     }
